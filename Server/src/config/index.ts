cc1a0e7c54e423f6e271069d79e69acf4fd58321
askoki160 <arian.skoki160@gmail.com>
2020-06-24 18:40:37 +0200

    Add imageKey to Restaurant and added timestamp in menu

--- Server/src/config/index.ts
+++ Server/src/config/index.ts
@@ -2,7 +2,7 @@ const appEnv = process.env.APP_ENV || 'development';
 
 const config = {
   env: appEnv,
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8080,
 };
 
 Object.assign(config, require(`./${config.env}`).default);
