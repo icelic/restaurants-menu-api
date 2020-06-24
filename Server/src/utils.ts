db0e57cc0e802532ded5459c20267e2bdce80bc5
askoki160 <arian.skoki160@gmail.com>
2020-06-24 14:15:36 +0200

    Fix import and add todo for location and key parameters

--- Server/src/utils.ts
+++ Server/src/utils.ts
@@ -1,4 +1,6 @@
-const AWS = require('aws-sdk');
+import AWS from 'aws-sdk';
+import { PutObjectRequest } from 'aws-sdk/clients/s3';
+
 const s3 = new AWS.S3();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -15,12 +17,14 @@ export function uploadToS3(
     Key: 'menupls' + '/' + folderName + '/' + fileName,
     Body: fileStream.buffer,
   };
-
   return new Promise((resolve, reject) => {
-    s3.upload(params, function (err, data) {
+    s3.upload(params as PutObjectRequest, function (err, data) {
       if (err) {
         return reject(err);
       }
+      //   TODO: Save values in the database
+      console.log('## location', data.Location);
+      console.log('## key', data.Key);
 
       return resolve(data);
     });
