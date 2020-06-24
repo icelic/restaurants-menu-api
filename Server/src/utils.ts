54eda6e7104eaadf8b5bc60693ee00a37701a04a
askoki160 <arian.skoki160@gmail.com>
2020-06-24 11:20:53 +0200

    Add utils helper method for uploading to s3 and add dependencies in package.json

---
+++ Server/src/utils.ts
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+export function uploadToS3(
+  fileStream: Buffer,
+  folderName: string,
+  fileName: string,
+): Promise<any> {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: 'menupls' + '/' + folderName + '/' + fileName,
+    Body: fileStream.buffer,
+  };
+
+  return new Promise((resolve, reject) => {
+    s3.upload(params, function (err, data) {
+      // readStream.destroy();
+
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(data);
+    });
+  });
+}
