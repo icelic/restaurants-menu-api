25722616b9bcac7118a1c8e96c8497d29d196cd1
askoki160 <arian.skoki160@gmail.com>
2020-06-25 19:03:10 +0200

    Add background image for header

--- Website/menupls-web/src/App.js
+++ Website/menupls-web/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App">MenuPls</div>;
+  return <div className="header-section">MenuPls</div>;
 }
 
 export default App;
