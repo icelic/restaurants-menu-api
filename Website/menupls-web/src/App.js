c26ac21c28d1479892461f5262dda8527e22d072
askoki160 <arian.skoki160@gmail.com>
2020-06-26 12:48:38 +0200

    Add basic css and structure for header section

--- Website/menupls-web/src/App.js
+++ Website/menupls-web/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./App.scss";
+import appLogo from "./images/app_logo.png";
 
 function App() {
   return (
     <div className="container">
-      <div className="header-section">MenuPls</div>
+      <div className="header-section">
+        <div className="header-content">
+          <div className="menu-icon-row">
+            <img className="image" src={appLogo} alt="App icon" />
+          </div>
+          <div className="title-row">
+            <div className="app-name">MenuPls</div>
+            <div className="header-title">
+              Life is like a menu, full of choices
+            </div>
+            <div className="header-description">
+              Check restaurant and bar prices in no time
+            </div>
+          </div>
+          <div className="store-button-row"></div>
+        </div>
+      </div>
     </div>
   );
 }
