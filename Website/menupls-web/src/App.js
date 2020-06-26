dd996589263d2ec97591273add724b585d7c8920
askoki160 <arian.skoki160@gmail.com>
2020-06-26 12:50:34 +0200

    Add app name prefix in css classes

--- Website/menupls-web/src/App.js
+++ Website/menupls-web/src/App.js
@@ -4,22 +4,22 @@ import appLogo from "./images/app_logo.png";
 
 function App() {
   return (
-    <div className="container">
-      <div className="header-section">
-        <div className="header-content">
-          <div className="menu-icon-row">
-            <img className="image" src={appLogo} alt="App icon" />
+    <div className="mp-container">
+      <div className="mp-header-section">
+        <div className="mp-header-content">
+          <div className="mp-menu-icon-row">
+            <img className="mp-image" src={appLogo} alt="App icon" />
           </div>
-          <div className="title-row">
-            <div className="app-name">MenuPls</div>
-            <div className="header-title">
+          <div className="mp-title-row">
+            <div className="mp-app-name">MenuPls</div>
+            <div className="mp-header-title">
               Life is like a menu, full of choices
             </div>
-            <div className="header-description">
+            <div className="mp-header-description">
               Check restaurant and bar prices in no time
             </div>
           </div>
-          <div className="store-button-row"></div>
+          <div className="mp-store-button-row"></div>
         </div>
       </div>
     </div>
