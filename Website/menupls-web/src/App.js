7df73b73f5e8d56cb512cc885a23fb9009418f7e
askoki160 <arian.skoki160@gmail.com>
2020-06-29 22:28:37 +0200

    Refactor app component and split into sections

--- Website/menupls-web/src/App.js
+++ Website/menupls-web/src/App.js
@@ -1,70 +1,15 @@
 import React from "react";
 import "./App.scss";
-import appLogo from "./images/app_logo.png";
-import mapImg from "./images/map.png";
-import venueImg from "./images/venue.png";
-import menuImg from "./images/menu.png";
-import appLogoBig from "./images/app_logo_big.png";
+import HeaderSection from "./components/HeaderSection";
+import GuideSection from "./components/GuideSection";
+import ConclusionSection from "./components/ConclusionSection";
 
 function App() {
   return (
     <div className="mp-container">
-      <div className="mp-header-section">
-        <div className="mp-header-content">
-          <div className="mp-menu-icon-row">
-            <img className="mp-image" src={appLogo} alt="App icon" />
-          </div>
-          <div className="mp-title-row">
-            <div className="mp-app-name">MenuPls</div>
-            <div className="mp-header-title">
-              Life is like a menu, full of choices
-            </div>
-            <div className="mp-header-description">
-              Check restaurant and bar prices in no time
-            </div>
-          </div>
-          <div className="mp-store-button-row"></div>
-        </div>
-      </div>
-      <div className="mp-guide-section">
-        <div className="mp-guide-title">How does it work?</div>
-        <div className="mp-guide-steps-container">
-          <div className="mp-maps-container">
-            <img className="mp-image" src={mapImg} alt="App map" />
-            <div className="mp-maps-description">
-              Filter results by given region or search for your desired place
-            </div>
-          </div>
-          <div className="mp-pick-venue-container">
-            <img className="mp-image" src={venueImg} alt="App venue" />
-            <div className="mp-pick-venue-description">
-              Pick the venue you are interested in
-            </div>
-          </div>
-          <div className="mp-menu-container">
-            <img className="mp-image" src={menuImg} alt="App menu" />
-            <div className="mp-menu-description">
-              Check the menu for your selection
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mp-conclusion-section">
-        <div className="mp-left-column">
-          <div className="mp-column-wrapper">
-            <div className="mp-title">
-              No more pressure when the waiter comes!
-            </div>
-            <img className="mp-image" src={appLogoBig} alt="App logo" />
-            <div className="mp-hint">
-              Goes hand in hand with the current COVID-19 situation too!
-            </div>
-          </div>
-        </div>
-        <div className="mp-right-column">
-          <div className="mp-title">You are always ready!</div>
-        </div>
-      </div>
+      <HeaderSection></HeaderSection>
+      <GuideSection></GuideSection>
+      <ConclusionSection></ConclusionSection>
     </div>
   );
 }
