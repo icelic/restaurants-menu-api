c5e236fcc3dbead1577e3cca97c4c8bc9f50560d
askoki160 <arian.skoki160@gmail.com>
2020-06-29 15:49:40 +0200

    Add guid section for desktop devices

--- Website/menupls-web/src/App.js
+++ Website/menupls-web/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.scss";
 import appLogo from "./images/app_logo.png";
+import mapImg from "./images/map.png";
+import venueImg from "./images/venue.png";
+import menuImg from "./images/menu.png";
 
 function App() {
   return (
@@ -20,6 +23,29 @@ function App() {
             </div>
           </div>
           <div className="mp-store-button-row"></div>
+        </div>
+      </div>
+      <div className="mp-guide-section">
+        <div className="mp-guide-title">How does it work?</div>
+        <div className="mp-guide-steps-container">
+          <div className="mp-maps-container">
+            <img className="mp-image" src={mapImg} alt="App map" />
+            <div className="mp-maps-description">
+              Filter results by given region or search for your desired place
+            </div>
+          </div>
+          <div className="mp-pick-venue-container">
+            <img className="mp-image" src={venueImg} alt="App venue" />
+            <div className="mp-pick-venue-description">
+              Pick the venue you are interested in
+            </div>
+          </div>
+          <div className="mp-menu-container">
+            <img className="mp-image" src={menuImg} alt="App menu" />
+            <div className="mp-menu-description">
+              Check the menu for your selection
+            </div>
+          </div>
         </div>
       </div>
     </div>
