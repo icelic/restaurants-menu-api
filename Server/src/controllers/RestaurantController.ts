5032e3903d4f2a884dea64357a0843e61ddf03bd
askoki160 <arian.skoki160@gmail.com>
2020-07-06 22:14:35 +0200

    Add ssl to elastic search client, add logs for env vars

--- Server/src/controllers/RestaurantController.ts
+++ Server/src/controllers/RestaurantController.ts
@@ -2,34 +2,45 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { Restaurant } from '../models/Restaurant';
 import { uploadToS3 } from '../utils';
+import fs from 'fs';
 
-// TODO decide where to put client node 
+// TODO decide where to put client node
 // TODO define host and port in a different file
-import { Client } from '@elastic/elasticsearch'
-const client = new Client({ node: process.env.API_HOST })
+import { Client } from '@elastic/elasticsearch';
+const client = new Client({
+  node: process.env.API_HOST,
+  auth: {
+    username: 'elastic',
+    password: process.env.ES_PASSWORD,
+  },
+  ssl: {
+    ca: fs.readFileSync('./certs/fullchain.pem'),
+    rejectUnauthorized: false,
+  },
+});
+console.log('Password: ', process.env.ES_PASSWORD);
+console.log('Host: ', process.env.API_HOST);
 
 class RestaurantController {
   async find(request: Request, response: Response) {
     // retrieve data from elastic search if query value is defined
     if (request.query.value) {
-      return client.search({
-        // TODO define indexes and fields in a different file
-        index: 'restaurants',
-        body: {
-          query: {
-            query_string: {  
-              fields:[  
-                'label',
-                'location'
-              ],
-              // find everything that contains the given value
-              query: '*' + request.query.value + '*'
-            }
-          }
-        }
-      })
-      .then((data) => response.json(data.body.hits.hits))
-      .catch((error) => console.log(error))
+      return client
+        .search({
+          // TODO define indexes and fields in a different file
+          index: 'restaurants',
+          body: {
+            query: {
+              query_string: {
+                fields: ['label', 'location'],
+                // find everything that contains the given value
+                query: '*' + request.query.value + '*',
+              },
+            },
+          },
+        })
+        .then((data) => response.json(data.body.hits.hits))
+        .catch((error) => console.log(error));
     }
 
     const restaurantRepository = getRepository(Restaurant);
@@ -46,18 +57,19 @@ class RestaurantController {
   }
 
   async saveToIndex(request: Request, response: Response) {
-    client.index({
-      // TODO define indexes in a different file
-      index: 'restaurants',
-      body: {
-        label: request.body.label, 
-        location: request.body.location,
-        imageKey: request.body.imageKey,
-        menus: request.body.menus,
-        foodType: request.body.foodType,
-      }
-    })
-    .then((data) => response.json(data.body))
+    client
+      .index({
+        // TODO define indexes in a different file
+        index: 'restaurants',
+        body: {
+          label: request.body.label,
+          location: request.body.location,
+          imageKey: request.body.imageKey,
+          menus: request.body.menus,
+          foodType: request.body.foodType,
+        },
+      })
+      .then((data) => response.json(data.body));
   }
 
   async uploadRestaurantImage(request: Request, response: Response) {
