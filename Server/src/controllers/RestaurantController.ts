00ee90c2ac0c7ac50d6b92331ad15006e71fe4e0
askoki160 <arian.skoki160@gmail.com>
2020-07-07 11:25:14 +0200

    Add typescript check to elastic search password

--- Server/src/controllers/RestaurantController.ts
+++ Server/src/controllers/RestaurantController.ts
@@ -11,7 +11,7 @@ const client = new Client({
   node: process.env.API_HOST,
   auth: {
     username: 'elastic',
-    password: process.env.ES_PASSWORD,
+    password: process.env.ES_PASSWORD!,
   },
   ssl: {
     ca: fs.readFileSync('./certs/fullchain.pem'),
