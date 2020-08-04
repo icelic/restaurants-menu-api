d1f1c686b19df17729f5660771d06bd16d285db8
Ivan Celic <icelic2@gmail.com>
2020-08-04 14:21:56 +0200

    Add option to add menu images when creating restaurant in admin

--- Server/src/routes/admin.ts
+++ Server/src/routes/admin.ts
@@ -3,14 +3,19 @@ import express, { Router } from 'express';
 import AdminController from '../controllers/AdminController';
 import { upload } from '../utils/upload';
 
+const imageUpload = upload.fields([
+  { name: 'restaurantImage', maxCount: 1 },
+  { name: 'menuImages' },
+]);
+
 const adminRouter: Router = express.Router();
 
 adminRouter.post('/login', AdminController.login);
 
 adminRouter.post(
   '/restaurants',
-  upload.single('image'),
-  AdminController.createRestaurant,
+  imageUpload,
+  AdminController.createRestaurantAndMenus,
 );
 
 export default adminRouter;
